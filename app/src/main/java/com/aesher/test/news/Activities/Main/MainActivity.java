ae5af88a8268a7934dc943b541552b804823d313
aeser9o1 <aashiskumar986@gmail.com>
2019-07-03 06:30:33 +0530

    Initial commit

---
+++ app/src/main/java/com/aesher/test/news/Activities/Main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.aesher.test.news.Activities.Main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aesher.test.news.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
